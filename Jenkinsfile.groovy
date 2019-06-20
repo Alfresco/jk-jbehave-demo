d84e7df0f7447000f9ddefe7a5fc93ed70b1daf5
Jamal Kaabi-Mofrad <jamal.kaabimofrad@alfresco.com>
2019-06-20 18:36:04 +0100

    Fixed Maven settings.

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -9,7 +9,7 @@ node {
     stage('Build') {
         configFileProvider(
                 [configFile(fileId: '78d61d7c-0a3a-4da5-974f-44fc98b78556', variable: 'MAVEN_SETTINGS')]) {
-            sh 'mvn -s $MAVEN_SETTINGS clean install'
+            sh "'${mvnHome}/bin/mvn' -s $MAVEN_SETTINGS clean install"
         }
 
     }
