0b3490eeeb36cfee86056903e24216dfcd62c57c
Jamal Kaabi-Mofrad <jamal.kaabimofrad@alfresco.com>
2019-06-20 15:32:20 +0100

    Added Maven settings.

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,11 +3,22 @@ node {
 
     stage('Preparation') {
         git 'https://github.com/Alfresco/jk-jbehave-demo.git'
-        mvnHome = tool 'M3'
+        mvnHome = tool 'M3',
+        mavenSettingsConfig: 'my-maven-settings'
     }
 
     stage('Build') {
-        sh "'${mvnHome}/bin/mvn' clean install"
+        withMaven(
+                // Maven settings.xml file defined with the Jenkins Config File Provider Plugin
+                // Maven settings and global settings can also be defined in Jenkins Global Tools Configuration
+                mavenSettingsConfig: 'my-maven-settings',
+                mavenLocalRepo: '.repository') {
+
+            // Run the maven build
+            sh "'${mvnHome}/bin/mvn' clean install"
+
+        }
+
     }
 
     stage('Results') {
